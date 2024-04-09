24be291aa29e1429ac11a8651b0d30d4ec8c7ee2
Mumlul <152785774+Mumlul@users.noreply.github.com>
2024-04-09 23:32:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+menu.onclick = function name() {
+    var x =document.getElementById("myTopnav")
+
+    if(x.className === "topnav"){
+        x.className+="responsive"
+
+    }else{
+        x.className ="topnav"
+    }
+}
